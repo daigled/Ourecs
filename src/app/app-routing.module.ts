e071f6b9175fd704c8128982933538880ac8c842
Daigle, Drew <drew.daigle.contractor@bbva.com>
2020-02-25 12:27:26 -0600

    nav component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RegisterComponent } from './components/register/register.component';
+import { VerifyComponent } from './components/verify/verify.component';
+import { LoginComponent } from './components/login/login.component';
+import { LandingComponent } from './components/landing/landing.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'register', component: RegisterComponent },
+  { path: 'verify', component: VerifyComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '',  component: LandingComponent }
+  // {path: '**': component: 404Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,23 @@ import { AppComponent } from './app.component';
 import { ArtistsComponent } from './components/artists/artists.component';
 import { TagsComponent } from './components/tags/tags.component';
 import { AlbumsComponent } from './components/albums/albums.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { NavComponent } from './components/nav/nav.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { VerifyComponent } from './components/verify/verify.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ArtistsComponent,
     TagsComponent,
-    AlbumsComponent
+    AlbumsComponent,
+    LandingComponent,
+    NavComponent,
+    RegisterComponent,
+    LoginComponent,
+    VerifyComponent
   ],
   imports: [
     BrowserModule,
